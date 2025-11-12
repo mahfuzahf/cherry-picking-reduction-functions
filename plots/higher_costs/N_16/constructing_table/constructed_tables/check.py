7bba41906ec355ab2ef7da7ea71ab2df9b69d421
Mahfuzah <mahfuzahf@outlook.com>
2025-11-12 12:01:41 +0000

    Generated different cherry tables and stored results of generating and searching them.

---
+++ plots/higher_costs/N_16/constructing_table/constructed_tables/check.py
@@ -0,0 +1,13 @@
+# count how many unique reduction function indexes each table has
+
+# imports
+import pickle
+
+cost_factors = list(range(5, 31, 5))
+
+for item in cost_factors:
+    with open(f'cherry_indexes_alpha_0.95_t_80_cost_{item}.pkl', 'rb') as f:
+        indexes = pickle.load(f)
+        f.close()
+    print(len(set(indexes)))
+    # print(indexes)
